4dcd69bc0b4cf7114171db1527255a8e376dbeb7
Cerozob <55302468+Cerozob@users.noreply.github.com>
2021-03-13 06:02:18 -0500

    j

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const body = document.body;
-const products = new Map(); //todo el catálogo
+const products = new Map(); //todo el catálogo .get(id): {product:objeto producto,node:objeto nodo}
+const categories = []; //lista de categorías, se usa cuando no hay productos en el resultado de búsqueda
 const favourites = new Map(); //productos agregados a favoritos
 //const datasisecaeGithub = "assets/products.json";
 const data = fetch(
@@ -39,13 +40,17 @@ function priceFormat(pricevalue /* precio en integer */) {
 	return output;
 }
 
-function createMarketplaceItemNode(productId /* id del producto */) {
-	let product = products.get(productId);
+function createMarketplaceItemNode(product /* objeto producto */) {
 	/* product picture */
-	let productPictureContainer = document.createElement("div");
-	productPictureContainer.className = "productpicturecontainer";
+	let productPictureButton = document.createElement("button");
+	productPictureButton.id = `${product.id}`;
+	productPictureButton.className = "productpicturebutton";
+	productPictureButton.addEventListener("click", (button) => {
+		displayProductDetail(button);
+	});
 	let productPicture = document.createElement("img");
 	productPicture.className = "productpicture";
+	productPicture.alt = `Picture of a ${product.title}`;
 	productPicture.src = product.picture;
 	/* free shipping icon */
 	let freeShippingIconSrc = "assets/shipping_free.png";
@@ -70,8 +75,8 @@ function createMarketplaceItemNode(productId /* id del producto */) {
 	/* card node */
 	let node = document.createElement("div");
 	node.className = "catalogitem";
-	productPictureContainer.appendChild(productPicture);
-	node.appendChild(productPictureContainer);
+	productPictureButton.appendChild(productPicture);
+	node.appendChild(productPictureButton);
 	node.appendChild(priceText);
 	node.appendChild(itemNameText);
 	node.appendChild(locationText);
@@ -81,28 +86,89 @@ function createMarketplaceItemNode(productId /* id del producto */) {
 	return node;
 }
 
-function displayCatalog(products /* objetos de productos */) {}
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function clearCatalog() {
+	let catalog = document.getElementById("catalog");
+	let catalogItems = catalog.children;
+	for (let i = 0; i < catalogItems.length; ) {
+		catalogItems.item(i).remove();
+	}
+}
+
+function displayCatalogItems() {
+	clearCatalog();
+	products.forEach((value) => {
+		catalog.appendChild(value.node);
+	});
+}
+
+function filterCatalogItems() {
+	let searchTerm = document.getElementById("searchbar").value;
+	let catalog = document.getElementById("catalog");
+	if (searchTerm.length === 0) {
+		displayCatalogItems();
+	} else {
+		clearCatalog();
+		products.forEach((value) => {
+			if (value.product.categories.includes(searchTerm)) {
+				catalog.appendChild(value.node);
+			}
+		});
+	}
+	if (catalog.children.length === 0) {
+		let notFound = document.createElement("p");
+		let searchAgain = document.createElement("p");
+		searchAgain.className = "searchagaintext";
+		notFound.className = "notfoundtext";
+		notFound.textContent = `Lo sentimos, no encontramos ningún producto de la categoría "${searchTerm}".`;
+		let randomSearchTerm = categories[random(0, categories.length)];
+		searchAgain.textContent = `Prueba con buscar "${randomSearchTerm}".`;
+		catalog.appendChild(notFound);
+		catalog.appendChild(searchAgain);
+	}
+}
 
 function displayProductDetail(product /* id del producto */) {}
 
-function displayFavorites(products /* ids de productos */) {}
+function displayFavorites() {}
 
-function selectFavorite() {}
+function addToFavorites() {}
 
 function addtoCart() {}
 
 function loadData(data) {
 	let items = data["items"];
-	let catalog = document.getElementById("catalog");
 	for (i = 0; i < items.length; i++) {
 		let product = items[i];
+
+		product.categories.forEach((value) => {
+			if (!categories.includes(value)) {
+				categories.push(value);
+			}
+		});
+
 		let id = product.id;
-		products.set(id, product);
-		let node = createMarketplaceItemNode(id);
-		catalog.appendChild(node);
+		let node = createMarketplaceItemNode(product);
+		products.set(id, { product: product, node: node });
 	}
+	displayCatalogItems();
 }
 
 data.then((resp) => {
 	resp.json().then(loadData);
 });
+
+document
+	.getElementById("searchbutton")
+	.addEventListener("click", filterCatalogItems);
+
+/* document
+	.getElementById("addtofavoritesbutton")
+	.addEventListener("click", addToFavorites()); */
+
+document
+	.getElementById("wishlistbutton")
+	.addEventListener("click", displayFavorites);
